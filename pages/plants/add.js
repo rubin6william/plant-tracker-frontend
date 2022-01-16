ec102b8ee63c470d4ca6b1a079c215796bf2d0e1
Rubin William <rubin6william@gmail.com>
2022-01-16 22:59:36 +1100

    Add plant add page

---
+++ pages/plants/add.js
@@ -0,0 +1,88 @@
+import Head from 'next/head'
+import MdEditor from 'react-markdown-editor-lite'
+import 'react-markdown-editor-lite/lib/index.css'
+import React from 'react'
+import Button from '../../components/button'
+import Input from '../../components/input'
+import FileInput from '../../components/file-input'
+
+// initialize markdown parser
+const MarkdownIt = require('markdown-it')
+const mdParser = new MarkdownIt()
+
+class AddPlant extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: '',
+      species: '',
+      watering_instructions: '',
+      photo: null
+    }
+
+    this.handleInputChange = this.handleInputChange.bind(this)
+    this.handleEditorChange = this.handleEditorChange.bind(this)
+    this.handleFileChange = this.handleFileChange.bind(this)
+  }
+
+  handleEditorChange({ html, text }) {
+    this.setState({watering_instructions: text})
+  }
+
+  handleInputChange(event) {
+    const target = event.target
+    const id = target.id
+
+    this.setState({
+      [id]: target.value
+    })
+  }
+
+  handleFileChange(event) {
+    this.setState({
+      photo: event.target.files[0]
+    })
+  }
+
+  render () {
+    return (
+      <>
+        <Head>
+          <title>Plant Tracker - Add Plant</title>
+        </Head>
+
+        <main className="px-6 py-4">
+          <div className="mx-auto border-gray-300 shadow-sm bg-white rounded-md px-6 py-4">
+            <h1 className="text-2xl font-bold mb-2">Add new plant</h1>
+
+            <div className="mb-4">
+              <label htmlFor="name">Name</label>
+              <Input id="name" onChange={this.handleInputChange}></Input>
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="species">Species</label>
+              <Input id="species" onChange={this.handleInputChange}></Input>
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="watering_instructions">Watering Instructions</label>
+              <MdEditor id="watering_instructions" style={{ height: '500px' }}
+                        renderHTML={text => mdParser.render(text)}
+                        onChange={this.handleEditorChange} />
+            </div>
+
+            <div className="mb-4">
+              <label htmlFor="photo"></label>
+              <FileInput onChange={this.handleFileChange}></FileInput>
+            </div>
+
+            <Button size="sm">Save</Button>
+          </div>
+        </main>
+      </>
+    )
+  }
+}
+
+export default AddPlant
